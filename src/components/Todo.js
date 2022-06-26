1758c343e267d97a52fa0cb2b434afefb072cf69
Adrion10 <lacehila@gmail.com>
2022-06-26 15:52:47 +0200

    Form

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Todo = () => {
-  return <div>Todo</div>;
+  return (
+    <div>
+      <h1>TODO Aplication</h1>
+      <form>
+        <label htmlFor="id">Input Item</label>
+        <imput id="id" />
+        <button> Add</button>
+      </form>
+    </div>
+  );
 };
 
 export default Todo;
